a5c2a0aa8e2702263260c0c9151f839102d53991
baxromjonov14@gmail.com <Baxromjonov14@gmail.com>
2025-02-23 11:50:38 +0500

    taxrir

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ var text
 
 var qoshuv = +prompt("Misolni yeching 1:  9 + 7 = ?");
 if (qoshuv==16) {console.log(`Sizning javobingiz to'g'ri: ${qoshuv}`);
-}else if (qoshuv) {console.log(`Sizning javobingiz noto'g'ri: ${qoshuv} (To'g'ri javob: 16)` );
+}else (qoshuv) {console.log(`Sizning javobingiz noto'g'ri: ${qoshuv} (To'g'ri javob: 16)` );
 }
 var ayruv = +prompt("Misolni yeching 2:  6 - 4 = ?");
 if (ayruv==2) {console.log(`Sizning javobingiz to'g'ri: ${ayruv}`);
-}else if (ayruv) {console.log(`Sizning javobingiz noto'g'ri: ${ayruv} (To'g'ri javob: 2)` );
+}else (ayruv) {console.log(`Sizning javobingiz noto'g'ri: ${ayruv} (To'g'ri javob: 2)` );
 }
 var kopaytiruv = +prompt("Misolni yeching 3:   8 * 6 = ?");
 if (kopaytiruv==48) {console.log(`Sizning javobingiz to'g'ri: ${kopaytiruv}`);
-}else if (kopaytiruv) {console.log(`Sizning javobingiz noto'g'ri: ${kopaytiruv} (To'g'ri javob: 48)` );
+}else (kopaytiruv) {console.log(`Sizning javobingiz noto'g'ri: ${kopaytiruv} (To'g'ri javob: 48)` );
 }
 var boluv = +prompt("Misolni yeching 4:   36 / 6 = ?");
 if (boluv==6) {console.log(`Sizning javobingiz to'g'ri: ${boluv}`);
-}else if (boluv) {console.log(`Sizning javobingiz noto'g'ri: ${boluv} (To'g'ri javob: 6)` );
+}else (boluv) {console.log(`Sizning javobingiz noto'g'ri: ${boluv} (To'g'ri javob: 6)` );
 }
 var foiz = +prompt("Misolni yeching 5:   52 % 3 = ?");
 if (foiz==1) {console.log(`Sizning javobingiz to'g'ri: ${foiz}`);
-}else if (foiz) {console.log(`Sizning javobingiz noto'g'ri: ${foiz} (To'g'ri javob: 1)` );
+}else (foiz) {console.log(`Sizning javobingiz noto'g'ri: ${foiz} (To'g'ri javob: 1)` );
 }
 var text = alert("Console oynasiga o'tib tekshirishingiz mumkin.");
